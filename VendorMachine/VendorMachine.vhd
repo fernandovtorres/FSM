de4d8bc391e76288789216f7a1c030cafe9e8f60
fernandovtorres <chereibos@gmail.com>
2024-11-03 17:20:31 -0300

    ata entendi

--- VendorMachine/VendorMachine.vhd
+++ VendorMachine/VendorMachine.vhd
@@ -4,35 +4,33 @@ use ieee.numeric_std.all;
 
 entity VendorMachine is
   port (
-      clk, clk_fpga: std_logic;
-      reset: in bit;
-      button: in bit;
-      coin: in bit_vector(2 downto 0);
-
-      current: out unsigned(4 downto 0) := (others => '0');
-      sodaDrink: out bit := '0';
-      exchange: out bit := '0';
-      stateOut: out bit := '0'
-       );
+      clk, clk_fpga : in std_logic;
+      reset : in std_logic;
+      button : in std_logic;
+      coin : in std_logic_vector(2 downto 0);
+      
+      current : out unsigned(4 downto 0);
+      sodaDrink : out std_logic;
+      exchange : out std_logic;
+      stateOut : out std_logic
+  );
 end VendorMachine;
 
 architecture behaviour of VendorMachine is
   type states is (noMoney, money);
-  signal state: states;
-  signal currentMoney: unsigned(4 downto 0) := (others => '0');
+  signal state : states := noMoney;
+  signal currentMoney : unsigned(4 downto 0) := (others => '0');
+  signal clock : std_logic;
 
-  signal clock: std_logic;
-  
   component Debouncing_Button_VHDL is
-    port(
-          button: in std_logic;
-          clk: in std_logic;
-          
-          debounced_button: out std_logic
-        );
+    port (
+        button : in std_logic;
+        clk : in std_logic;
+        debounced_button : out std_logic
+    );
   end component;
 
-  function coinValue(coin : in bit_vector(2 downto 0))
+  function coinValue(coin : in std_logic_vector(2 downto 0))
     return unsigned is
       variable outValue : unsigned(4 downto 0) := (others => '0');
   begin
@@ -41,32 +39,64 @@ architecture behaviour of VendorMachine is
       when "010" => outValue := outValue + 5;
       when "011" => outValue := outValue + 10;
       when "100" => outValue := outValue + 20;
-      when others => outValue := outValue;
+      when others => null;
     end case;
     return outValue;
   end function coinValue;
+
 begin
   debouncer: Debouncing_Button_VHDL
     port map (
-                button => clk, clk => clk_fpga, debounced_button => clock
-             );
-  process(clock)
+      button => button,
+      clk => clk_fpga,
+      debounced_button => clock
+    );
+
+  process(clock, reset, button)
   begin
-    if(reset = '1') then
+    if reset = '1' then
       state <= noMoney;
       sodaDrink <= '0';
       exchange <= '0';
       stateOut <= '0';
-      current <= "00000";
-      currentMoney <= "00000";
+      current <= (others => '0');
+      currentMoney <= (others => '0');
 
-    elsif (clock'event) and (clock = '1') then
+    elsif currentMoney + coinValue(coin) > 20 then
+      state <= noMoney;
+      sodaDrink <= '0';
+      exchange <= '1';
+      current <= (others => '0');
+      currentMoney <= (others => '0');
+      stateOut <= '0';
+
+    elsif button = '1' then  
+      if currentMoney + coinValue(coin) = 20 then
+        state <= noMoney;
+        sodaDrink <= '1';
+        exchange <= '0';
+        current <= (others => '0');
+        currentMoney <= (others => '0');
+        stateOut <= '0';
+
+      elsif currentMoney + coinValue(coin) < 20 then
+        state <= noMoney;
+        sodaDrink <= '0';
+        exchange <= '1';
+        current <= (others => '0');
+        currentMoney <= (others => '0');
+        stateOut <= '0';
+
+      end if;
+
+
+    elsif rising_edge(clock) then
       case state is
-        when noMoney => 
-          if (coin /= "000") then 
+        when noMoney =>
+          if coin /= "000" then
             state <= money;
-            current <= currentMoney + coinValue(coin);
             currentMoney <= currentMoney + coinValue(coin);
+            current <= currentMoney;
             stateOut <= '1';
           else
             sodaDrink <= '0';
@@ -74,31 +104,8 @@ begin
           end if;
 
         when money =>
-          if(currentMoney + coinValue(coin) = 20 and button = '1') then
-            state <= noMoney;
-            stateOut <= '0';
-            sodaDrink <= '1';
-            current <= "00000";
-            currentMoney <= "00000";
-
-          elsif(currentMoney + coinValue(coin) < 20 and button = '1') then
-            state <= noMoney;
-            stateOut <= '0';
-            exchange <= '1';
-            current <= "00000";
-            currentMoney <= "00000";
-
-          elsif(currentMoney + coinValue(coin) > 20) then 
-            state <= noMoney;
-            stateOut <= '0';
-            exchange <= '1';
-            current <= "00000";
-            currentMoney <= "00000";
-
-          else 
-            current <= currentMoney + coinValue(coin);
-            currentMoney <= currentMoney + coinValue(coin);
-          end if;
+          currentMoney <= currentMoney + coinValue(coin);
+          current <= currentMoney;
       end case;
     end if;
   end process;
